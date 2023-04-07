209da4ec338378378028f68b1f38ee8b818aa636
Diego Rodriguez <dsrrdiego@hotmail.com>
2023-04-07 16:26:33 -0300

    primero

---
+++ clases.js
@@ -0,0 +1,92 @@
+class Figura{
+    
+    constructor(x,y,fill,ctx){
+        this.x=x;
+        this.y=y;
+        this.fill=fill;
+        this.ctx=ctx;
+        this.ctx.lineWidth=4;
+        this.ctx.strokeStyle='red';
+    }
+    dibujar(){
+        this.ctx.fillStyle=this.fill;
+    }
+    // checkMouse(){}
+    
+    
+}
+class Circulo extends Figura{
+    constructor(x,y,radio,fill,ctx){
+        super (x,y,fill,ctx);
+        this.radio=radio;
+    }
+    dibujar(){
+        super.dibujar();
+        this.ctx.beginPath();
+        this.ctx.arc(this.x,this.y,this.radio,0,2*Math.PI);
+        this.ctx.fill();
+        this.ctx.closePath();
+    }
+    marcar(){
+        super.dibujar();
+        this.ctx.beginPath();
+        this.ctx.arc(this.x,this.y,this.radio,0,2*Math.PI);
+        // this.ctx.lineWidth=this.borde;
+        // this.ctx.lineCap='round';
+        // this.ctx.strokeStyle='red';
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+    // borrar(){
+    //     this.ctx.fillStyle='#000000'
+    //     this.ctx.beginPath();
+    //     this.ctx.arc(this.x,this.y,this.radio+this.borde/2+1,0,2*Math.PI);
+    //     this.ctx.fill();
+    //     this.ctx.closePath();
+    // }
+
+
+
+    checkMouse(x,y){
+        // console.log('toquing'+ Math.sqrt( x-this.x*x-this.x+y-this,2) ) )
+        if (!seleccionado){
+            if (Math.sqrt(Math.pow(x-this.x,2)+Math.pow(y-this.y,2))<this.radio){
+                  this.marcar();
+                  
+                 return true;
+         }}else {
+            
+            return false;
+    
+        }
+    }
+}
+
+class Rectangulo extends Figura{
+    constructor(x,y,ancho,alto,fill,ctx){
+        super (x,y,fill,ctx);
+        this.ancho=ancho;
+        this.alto=alto;
+    }
+    dibujar(){
+        super.dibujar();
+        this.ctx.fillRect(this.x,this.y,this.ancho,this.alto);
+    }
+
+    marcar(){
+        // this.ctx.strokeStyle="red";
+        // this.ctx.lineWidth=20;
+ 
+        
+        this.ctx.strokeRect(this.x,this.y,this.ancho,this.alto);
+    }
+
+    checkMouse(x,y){
+        if (x>this.x && x < this.x+this.ancho
+            && y>this.y && y<this.y+this.alto) {
+                this.marcar();
+                return true }
+            else {return false};
+
+        }
+}
---
+++ main.js
@@ -0,0 +1,128 @@
+const canvas=document.getElementById('canvas');
+const ctx=document.getElementById('canvas').getContext('2d');
+
+
+const CANVASANCHO=1000;
+const CANVASALTO=1000;
+const  figura=[];
+let seleccionado=false;
+
+canvas.addEventListener('mousemove',function(e){
+    borrar();
+    seleccionado=false;
+    for (x=0;x<10 && !seleccionado;x++){
+        if (figura[x].checkMouse(e.x,e.y)) {
+            seleccionado=true;
+            
+        }
+    }
+    
+    // console.log('mouser'+e.x+ '  '+e.y);
+})
+
+function borrar(){
+    ctx.fillStyle='#ffffff';
+    ctx.fillRect(0,0,CANVASALTO,CANVASANCHO);
+    refresh();
+}
+
+function refresh(){
+    figura.forEach(f =>f.dibujar());
+}
+
+
+function dibujarFiguraAleatoria(tipo, indice){
+    const x=Math.floor(Math.random()*CANVASANCHO-200);
+    const y=Math.floor(Math.random()*CANVASALTO-200);
+    const ancho=Math.floor(Math.random()*100)+20;
+    const alto=Math.floor(Math.random()*100)+20;
+    const colorR=Math.floor(Math.random()*200)+50;
+    const colorG=Math.floor(Math.random()*255);
+    const colorB=Math.floor(Math.random()*255);
+    const fill='#'+colorR+colorG+colorB;
+    
+    switch (tipo){
+        case 0:
+            figura[indice]=new Rectangulo(x,y,ancho,alto,fill,ctx);
+            break;
+            case 1:
+                figura[indice]=new Circulo(x,y,ancho,fill,ctx);
+                break;
+                case 2:
+                    // console.log(2);
+                    break;
+                    default:
+            // console.log('defa');
+            
+        }
+        // figura[indice].dibujar();
+        refresh();
+    }
+    
+borrar();
+for (i=0;i<10;i++){
+    const figuraTipo=Math.floor(Math.random()*2);
+    dibujarFiguraAleatoria(figuraTipo,i);
+}
+    
+    
+    
+    
+    
+/*
+const c1=new Circulo(20,20,10,'#0000FF',ctx);
+c1.dibujar();
+
+let grad1=ctx.createLinearGradient(10,90,200,90);
+grad1.addColorStop(0,'white');
+grad1.addColorStop(1,'blue');
+
+const r1=new Rectangulo(180,80,20,20,'#af7025',ctx);
+r1.dibujar();
+const r2=new Rectangulo(0,110,30,30,grad1,ctx);
+r2.dibujar();
+
+
+let img=new Image(100,100);
+img.src='img1.jpg';
+img.onload=function (){
+    let rellenoImagen=ctx.createPattern(img,'repeat');
+    
+    
+    const c2=new Circulo(200,200,80,rellenoImagen,ctx);
+    c2.dibujar(); 
+//     ctx.drawImage(img,100,100);
+}
+
+
+class rombo{
+
+    constructor(x,y,radio,ctxt){
+        let img=ctxt.getImageData(x,y,x+radio,y-radio);
+        console.log(img.width);
+        for (let yy=0;yy<radio/2;yy++){
+            
+            for (let xx=radio-yy;xx<radio+yy;xx++){
+                let index=(xx+yy*img.width)*4;
+                img.data[index]=0;
+                img.data[index+1]=250;
+                img.data[index+2]=0;
+            }
+        }
+        for (let yy=radio/2;yy<radio;yy++){
+            
+            for (let xx=20+radio+(yy-radio);xx<radio*2-(yy-radio)+radio/2;xx++){
+                let index=(xx+yy*img.width)*4;
+                img.data[index]=0;
+                img.data[index+1]=250;
+                img.data[index+2]=0;
+            }
+        }
+        
+        ctxt.putImageData(img,x-radio,y-radio);
+        // ctxt.putImageData(img,0,0);
+
+    }
+}
+const a =new rombo(50,50,25,ctx);
+*/
